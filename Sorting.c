5c52dbda66dbdd66db81e41f37c969bda426795a
Rohit <rv17837@outlook.com>
2022-12-07 00:09:26 +0530

    Insertion sort added

--- Sorting.c
+++ Sorting.c
@@ -4,13 +4,15 @@
 void printArr(int arr[], int size);
 void bubbleSort(int arr[], int size);
 void selectionSort(int arr[], int size);
+void insertionSort(int arr[], int size);
 
 
 int main(){
     int arr[] = {4,3,1,2,5};
     int size = sizeof(arr)/sizeof(arr[0]); // Size of array
     // bubbleSort(arr,size);
-    selectionSort(arr,size);
+    // selectionSort(arr,size);
+    insertionSort(arr, size);
     printArr(arr,size);
 }
 // Functions Definitions
@@ -45,5 +47,18 @@ void selectionSort(int arr[], int size){
         int temp = arr[minPos];
         arr[minPos] = arr[i];
         arr[i] = temp;
+    }
+}
+// Insertion sort
+void insertionSort(int arr[], int size){
+    for(int i=1; i<size; i++){
+        int curr = arr[i];
+        int prev = i-1;
+        while(prev >= 0 && arr[prev] > curr){
+            arr[prev+1] = arr[prev];
+            prev--;
+        }
+        //Insertion
+        arr[prev+1] = curr;
     }
 }
